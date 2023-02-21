904668a1a823b5b9162bf66fb77ddfbaea5d96ab
TARAK <tarakaramtirumala@gmail.com>
2023-02-21 15:08:44 +0530

    print in shell

---
+++ 05.function.sh
@@ -0,0 +1,18 @@
+## Declare a function
+
+xyz() {
+  echo Hello from Function
+  echo First Argument - $1
+  echo Second Argument -- $2
+  echo All Arguments - $*
+  echo No of Arguments - $#
+  a=300
+  echo Value of a = $a
+  b=200
+}
+
+## Main Program
+## Call a function
+a=120
+xyz 123 456
+echo Value of b - $b
