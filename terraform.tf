a3a87ea53968a4407c908bfaa18212795fc17950
TARAK <tarakaramtirumala@gmail.com>
2023-02-24 15:59:20 +0530

    changes

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,6 @@
-resource "aws_spot_instance_request" "tarak" {
+resource "aws_instance" "web" {
   ami           = "ami-0a017d8ceb274537d"
-    instance_type = "t3.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "tarak"
