16ecc8e68cc8b6af73c7418d66d8b0e5ea04b90e
TARAK <tarakaramtirumala@gmail.com>
2023-02-24 11:29:28 +0530

    changes

--- practice.sh
+++ practice.sh
@@ -1,3 +1,8 @@
-echo " a comment will follow"
-echo " a comment will follow"
-echo The # here begins a comment.
+
+
+
+if [ -x "$filename" ]; then
+  echo "File $filename exists."; cp $filename $filename.bak
+else
+  echo "File $filename not found."; touch $filename
+fi; echo "File test complete."
