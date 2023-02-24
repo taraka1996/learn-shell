8aafd9d9a40776d09b5bcf0666ff7ba90d800ded
TARAK <tarakaramtirumala@gmail.com>
2023-02-24 15:46:15 +0530

    changes

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "this" {
   ami                     = "ami-0a017d8ceb274537d"
   instance_type           = "t3.micro"
-    vpc_security_group_ids = "allow all"
+    vpc_security_group_ids = "sg-02811f5637016bdbd"
 }
