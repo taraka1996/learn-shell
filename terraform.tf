ec6772f1d28d20874233d1859bc1e08a601224b2
TARAK <tarakaramtirumala@gmail.com>
2023-02-24 15:40:48 +0530

    changes

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,5 @@
 resource "aws_instance" "this" {
   ami                     = "ami-0a017d8ceb274537d"
   instance_type           = "t3.micro"
-  host_resource_group_arn = "arn:aws:resource-groups:us-west-2:012345678901:group/win-testhost"
-  tenancy                 = "host"
+    vpc_security_group_ids = "allow all"
 }
