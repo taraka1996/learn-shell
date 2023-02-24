91307383640b3baba270790d1f644d8dd0075ef2
TARAK <tarakaramtirumala@gmail.com>
2023-02-24 15:56:32 +0530

    changes

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,8 @@
-resource "aws_instance" "this" {
-  ami                     = "ami-0a017d8ceb274537d"
-  instance_type           = "t3.micro"
-    vpc_security_group_ids = "sg-02811f5637016bdbd"
+resource "aws_spot_instance_request" "tarak" {
+  ami           = "ami-0a017d8ceb274537d"
+    instance_type = "t3.micro"
+
+  tags = {
+    Name = "tarak"
+  }
 }
